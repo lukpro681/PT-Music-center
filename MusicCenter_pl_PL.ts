c41831ca3621475cf112a19e00f470715f70d6f0
lukpro681 <lukapro681@student.polsl.pl>
2024-02-07 01:35:41 +0100

    ts

--- MusicCenter_pl_PL.ts
+++ MusicCenter_pl_PL.ts
@@ -2,6 +2,30 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="pl_PL">
 <context>
+    <name>Converter</name>
+    <message>
+        <location filename="converterBase.ui" line="14"/>
+        <location filename="converterwavmp3.ui" line="26"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="converterwavmp3.ui" line="32"/>
+        <source>Choose .wav file</source>
+        <translation>Wybierz plik .wav</translation>
+    </message>
+    <message>
+        <location filename="converterwavmp3.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <location filename="converterwavmp3.ui" line="49"/>
+        <source>Convert to .mp3</source>
+        <translation>Konwertuj na .mp3</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
@@ -9,42 +33,87 @@
         <translation>Music Center</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="54"/>
+        <location filename="mainwindow.ui" line="34"/>
+        <source>Playing:</source>
+        <translation>Odtwarzanie:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="55"/>
+        <source>Shuffle</source>
+        <translation>Losowo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="62"/>
+        <source>Previous</source>
+        <translation>Poprzedni</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="69"/>
         <source>Play</source>
         <translation>Odtwórz</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="67"/>
+        <location filename="mainwindow.ui" line="76"/>
+        <source>Next</source>
+        <translation>Następny</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="83"/>
         <source>Stop</source>
         <translation>Zatrzymaj</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="80"/>
+        <location filename="mainwindow.ui" line="90"/>
+        <source>Repeat</source>
+        <translation>Powtórz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
         <source>Add Folder(s)</source>
         <translation>Dodaj Folder</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="93"/>
+        <location filename="mainwindow.ui" line="155"/>
+        <source>.wav to .mp3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="160"/>
+        <source>.wav to .ogg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <source>Converter</source>
+        <translation>Konwerter plików dźwiękowych</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
         <source>Refresh</source>
         <translation>Odśwież</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="108"/>
+        <location filename="mainwindow.ui" line="126"/>
         <source>General</source>
         <translation>Ogólne</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="118"/>
+        <location filename="mainwindow.ui" line="133"/>
+        <source>Tools</source>
+        <translation>Narzędzia</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="145"/>
         <source>Settings</source>
         <translation>Ustawienia</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="123"/>
+        <location filename="mainwindow.ui" line="150"/>
         <source>About</source>
         <translation>O Aplikacji</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="62"/>
+        <location filename="mainwindow.cpp" line="101"/>
         <source>Select Music Folder</source>
         <translation>Wybierz folder</translation>
     </message>
@@ -57,22 +126,32 @@
         <translation>Ustawienia</translation>
     </message>
     <message>
-        <location filename="wsettings.ui" line="46"/>
+        <location filename="wsettings.ui" line="22"/>
+        <source>Supported formats</source>
+        <translation>Obsługiwane formaty plików:</translation>
+    </message>
+    <message>
+        <location filename="wsettings.ui" line="34"/>
+        <source>Searched folders</source>
+        <translation>Foldery muzyki:</translation>
+    </message>
+    <message>
+        <location filename="wsettings.ui" line="70"/>
         <source>Apply</source>
         <translation>Zastosuj</translation>
     </message>
     <message>
-        <location filename="wsettings.ui" line="59"/>
+        <location filename="wsettings.ui" line="63"/>
         <source>Cancel</source>
         <translation>Anuluj</translation>
     </message>
     <message>
-        <location filename="wsettings.ui" line="72"/>
+        <location filename="wsettings.ui" line="56"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="wsettings.ui" line="85"/>
+        <location filename="wsettings.ui" line="44"/>
         <source>Add Folder(s)</source>
         <translation>Dodaj folder</translation>
     </message>
@@ -80,6 +159,72 @@
         <location filename="wsettings.cpp" line="28"/>
         <source>Select Folder</source>
         <translation>Wybierz folder</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="about.ui" line="26"/>
+        <source>Dialog</source>
+        <translation>O Music Center</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="40"/>
+        <source>Authors: </source>
+        <translation>Autorzy:</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="47"/>
+        <source>Version</source>
+        <translation>Wersja</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="54"/>
+        <source>Authors</source>
+        <translation>Autorzy</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="61"/>
+        <source>License</source>
+        <translation>Licencja</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="71"/>
+        <source>App name</source>
+        <translation>Nazwa Aplikacji</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="78"/>
+        <source>Version:</source>
+        <translation>Wersja</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="110"/>
+        <source>OK</source>
+        <translation>OKOK</translation>
+    </message>
+</context>
+<context>
+    <name>converterWavOgg</name>
+    <message>
+        <location filename="converterwavogg.ui" line="26"/>
+        <source>Dialog</source>
+        <translation>Konwertuj na plik .ogg</translation>
+    </message>
+    <message>
+        <location filename="converterwavogg.ui" line="32"/>
+        <source>Choose .wav file</source>
+        <translation>Dodaj plik .wav</translation>
+    </message>
+    <message>
+        <location filename="converterwavogg.ui" line="42"/>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <location filename="converterwavogg.ui" line="49"/>
+        <source>Convert to .ogg</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
